0c652b925d7b56c985b986b54d82e1c807e85967
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-07-20 23:36:08 +0530

    EP-69

--- Demo.java
+++ Demo.java
@@ -10,14 +10,35 @@ enum Status{
 public class Demo{
     public static void main(String args[]){
        Status s=Status.Running;
-       System.out.println(s);   
+       
+       /* if(s==Status.Pending){
+        System.out.println("Currently pending task");
+       }
+       else if(s==Status.Running){
+        System.out.println("Now running task");
+       }
+       else if(s==Status.Success){
+        System.out.println("Task success !!!");
+       }
+       else{
+        System.out.println("Task Failed");
+       } */
 
-       Status s1=Status.Pending;
-       System.out.println(s1.ordinal());
-
-       Status arr[]=Status.values();
-       for(Status st:arr){
-            System.out.println(st+" "+st.ordinal());
-       } 
+       switch (s) {
+           case Pending:
+                System.out.println("Currently pending task");
+                break;
+            case Running:
+                System.out.println("Now running task");
+                break;
+            case Success:
+                System.out.println("Task success !!!");
+                break;
+            case Failed:
+                System.out.println("Task Failed");
+                break;
+           default:
+                System.out.println("WRONG STATUS");
+       }
     }
 }

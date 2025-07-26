13075f1d45b66edd63f09800177cbf9d5e914700
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-07-27 01:23:15 +0530

    EP-77

--- Demo.java
+++ Demo.java
@@ -1,26 +1,16 @@
-
-interface A{
-    int sum(int a, int b);
-}
-
-/* class B implements A{   // standard way to define a interface functions
-    public void show(){
-        System.out.println("in B Show()");
-    }
-} */
-
 public class Demo{
     public static void main(String args[]){
 
-        A obj2= (a,b) -> {
-            System.out.println("in A sum()");
-            return (a+b);
-        };
+        int a=10;
+        int res=-1;
+        int b=(int)(Math.random()*10);
 
-        A obj3=(a,b)->a+b;
+        try {
+            res=a/b;
+        } catch (Exception e) {
+            System.out.println("division NOT successful as deno is 0");
+        }
 
-        int res=obj2.sum(5,10);
-        int res1=obj3.sum(51, 49);
-        System.out.println("res = "+res+" res1 = "+res1);
+        System.out.println("division successful res = "+res);
     }
 }

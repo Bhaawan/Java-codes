29bfcdf7c3cecee4c6bb2f5e18922c1aced866a3
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-07-27 22:53:15 +0530

    EP-81

--- Demo.java
+++ Demo.java
@@ -1,22 +1,28 @@
+
+class BhaawanException extends Exception{
+
+    public BhaawanException(String str){
+        super(str);
+    }
+}
+
 public class Demo{
     public static void main(String args[]){
 
-        int a=10, res=-1, b=100;
+        int a=10, res=-1, b=20;
 
         try {
             res=a/b;
             if(res==0){
-                throw new ArithmeticException();
+                throw new BhaawanException( "I dont want result to be 0 :) setting res=a");
             }
         }
         catch (ArithmeticException e) {
-            if(b==0){
-                System.out.println("Cannot divide by 0");
-            }
-            else if(res==0){
-                res=a;
-                System.out.println("res can't be zero");
-            }
+            System.out.println("Cannot divide by 0");
+        }
+        catch (BhaawanException bh) {
+            res=a;
+            System.out.println("Bhaawan Exception recieved "+bh);
         }
         catch(Exception e){
             System.out.println("SOME WEIRD ANOMLY "+e);

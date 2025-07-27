663763a1387ae1d0f86fb181175ba26f2a2a3c07
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-07-27 22:12:46 +0530

    EP-80

--- Demo.java
+++ Demo.java
@@ -1,23 +1,22 @@
 public class Demo{
     public static void main(String args[]){
 
-        int a=10, res=-1, b=5;
-
-        int arr[]=new int[10];
-        int index=2;
-
-        String str=null;
+        int a=10, res=-1, b=100;
 
         try {
             res=a/b;
-            System.out.println(arr[index]);
-            System.out.println("length of string = "+str.length());
+            if(res==0){
+                throw new ArithmeticException();
+            }
         }
         catch (ArithmeticException e) {
-            System.out.println("Cannot divide by 0");
-        }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Requested index "+index+" is out of bounds for array of size "+arr.length);
+            if(b==0){
+                System.out.println("Cannot divide by 0");
+            }
+            else if(res==0){
+                res=a;
+                System.out.println("res can't be zero");
+            }
         }
         catch(Exception e){
             System.out.println("SOME WEIRD ANOMLY "+e);

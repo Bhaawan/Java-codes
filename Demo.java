35436615c42de0a5adae102084d6ac2b6815aee7
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-07-27 15:58:45 +0530

    EP-78

--- Demo.java
+++ Demo.java
@@ -1,14 +1,26 @@
 public class Demo{
     public static void main(String args[]){
 
-        int a=10;
-        int res=-1;
-        int b=(int)(Math.random()*10);
+        int a=10, res=-1, b=5;
+
+        int arr[]=new int[10];
+        int index=2;
+
+        String str=null;
 
         try {
             res=a/b;
-        } catch (Exception e) {
-            System.out.println("division NOT successful as deno is 0");
+            System.out.println(arr[index]);
+            System.out.println("length of string = "+str.length());
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by 0");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Requested index "+index+" is out of bounds for array of size "+arr.length);
+        }
+        catch(Exception e){
+            System.out.println("SOME WEIRD ANOMLY "+e);
         }
 
         System.out.println("division successful res = "+res);

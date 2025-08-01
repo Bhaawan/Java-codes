e973e0cd6d747e7f4043f2dbe24e39f1c2fc2bf3
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-08-01 19:23:57 +0530

    EP-84

--- Demo.java
+++ Demo.java
@@ -1,61 +1,36 @@
-<<<<<<< HEAD
 
-
-
-public class Demo{
-    public static void main(String args[]){
-        try {
-            a();
-            b();
-        } catch (ArithmeticException e) {
-            System.out.println("deno cant be zero "+e);
-        }
-    }
-
-    public static void a() throws ArithmeticException{
-        int i=(int)(Math.random()*10);
-        int x=10/i;
-
-        System.out.println("value of x = "+x);
-    }
-
-    public static void b() throws ArithmeticException{
-        int i=0;
-        int y=10/i;
-
-        System.out.println("value of y = "+y);
-=======
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Demo{
-    public static void main(String[] args) throws IOException{
-        // 1st way of taking a input in java OLD WAY
+    public static void main(String args[]) throws IOException{
+        //InputStreamReader in=new InputStreamReader(System.in);
         
-            System.out.print("hello world enter a number = ");
+        // 1st way of using try with finally for resources the long way
+        
+            BufferedReader bf=null;
 
-            InputStreamReader in=new InputStreamReader(System.in);
-            BufferedReader bf=new BufferedReader(in);
+            int num=0;
+            System.out.println("enter a number = ");
 
-            int num=-999;
-            num=Integer.parseInt(bf.readLine());
-            
-            System.out.println("entered number = "+num);
+            try{
+                bf=new BufferedReader(new InputStreamReader(System.in));
+                num=Integer.parseInt(bf.readLine());
+                System.out.println("number entered = "+num);
+            }
+            finally{
+                bf.close();    // mostly used to close resources...
+            }
+        
+        // 2nd way of using a shorter way
 
-            bf.close();
+            int num1=0;
+            System.out.println("enter a new number = ");
 
-        // 2nd way of taking input NEWER WAY
-
-            System.out.print("enter a number = ");
-
-            Scanner sc=new Scanner(System.in);
-            int num1=sc.nextInt();
-
-            System.out.println("new entered number = "+num1);
-
->>>>>>> 26158f65a5a5bb9f8a0909136ad7b5a8218f4e00
+            try(BufferedReader bf1=new BufferedReader(new InputStreamReader(System.in))){
+                num1=Integer.parseInt(bf1.readLine());
+                System.out.println("new number entered = "+num1);
+            }
     }
 }

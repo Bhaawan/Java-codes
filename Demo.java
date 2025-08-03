260449a26a2455346ef319ab8e104312271c77f5
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-08-04 00:36:07 +0530

    EP-87

--- Demo.java
+++ Demo.java
@@ -6,6 +6,11 @@ class A extends Thread{
     public void run(){
         for(int i=0; i<100; i++){
             System.out.println("hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -14,6 +19,11 @@ class B extends Thread{
     public void run(){
         for(int i=0; i<100; i++){
             System.out.println("hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -22,8 +32,18 @@ public class Demo{
     public static void main(String args[]) throws IOException{
         A obj1=new A();
         B obj2=new B();
+        
+        // obj1.setPriority(8);
+        // System.out.println(obj1.getPriority());
 
         obj1.start();
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         obj2.start();
     }
 }

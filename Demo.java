c64e779b0bb24a01c23540f273185a1cded80eaa
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-08-03 19:13:35 +0530

    EP-86

--- Demo.java
+++ Demo.java
@@ -1,36 +1,29 @@
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
+
+class A extends Thread{
+    public void run(){
+        for(int i=0; i<100; i++){
+            System.out.println("hi");
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for(int i=0; i<100; i++){
+            System.out.println("hello");
+        }
+    }
+}
 
 public class Demo{
     public static void main(String args[]) throws IOException{
-        //InputStreamReader in=new InputStreamReader(System.in);
-        
-        // 1st way of using try with finally for resources the long way
-        
-            BufferedReader bf=null;
+        A obj1=new A();
+        B obj2=new B();
 
-            int num=0;
-            System.out.println("enter a number = ");
-
-            try{
-                bf=new BufferedReader(new InputStreamReader(System.in));
-                num=Integer.parseInt(bf.readLine());
-                System.out.println("number entered = "+num);
-            }
-            finally{
-                bf.close();    // mostly used to close resources...
-            }
-        
-        // 2nd way of using a shorter way
-
-            int num1=0;
-            System.out.println("enter a new number = ");
-
-            try(BufferedReader bf1=new BufferedReader(new InputStreamReader(System.in))){
-                num1=Integer.parseInt(bf1.readLine());
-                System.out.println("new number entered = "+num1);
-            }
+        obj1.start();
+        obj2.start();
     }
 }

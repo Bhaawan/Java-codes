82349fec8364ff766617941b0876b1d7f1f6bca6
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-08-07 01:03:16 +0530

    EP-83 Part-2

--- Demo.java
+++ Demo.java
@@ -1,45 +1,35 @@
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-class Counter{
-    int count;
-
-    public synchronized void incrementCount(){
-        count++;
-    }
-}
 
 public class Demo{
-    public static void main(String args[]) throws IOException{
-
-        Counter c=new Counter();
-
-        Runnable obj1=()->{
-            for(int i=0; i<1000; i++){
-                c.incrementCount();
-            }
-        };
-
-        Runnable obj2=()->{
-            for(int i=0; i<1000; i++){
-                c.incrementCount();
-            }
-        };
-        
-        Thread t1=new Thread(obj1);
-        Thread t2=new Thread(obj2);
-
-        t1.start();
-        t2.start();
-
+    public static void main(String args[]){
+        // taking input from a file
+        File file=new File("./input.txt");
         try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            Scanner sc=new Scanner(file);
+            while(sc.hasNextLine()){
+                String line=sc.nextLine();
+                System.out.println(line);
+            }
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found "+e);
         }
-        
-        
-        System.out.println(c.count);
+
+        // taking input from a string
+        String str="123 78 5896 147";
+        try {
+            Scanner sc=new Scanner(str);
+            while(sc.hasNextLine()){
+                int num=sc.nextInt();
+                System.out.println(num);
+            }
+            
+        } catch (Exception e) {
+            System.out.println("A weird exception "+e);
+        }
     }
 }

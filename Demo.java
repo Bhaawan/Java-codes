7430f507f8a733e0efb0b4a95d6fffc68cc22eb0
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-09-08 00:23:29 +0530

    EP-92

--- Demo.java
+++ Demo.java
@@ -1,35 +1,41 @@
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 
 public class Demo{
     public static void main(String args[]){
-        // taking input from a file
-        File file=new File("./input.txt");
-        try {
-            Scanner sc=new Scanner(file);
-            while(sc.hasNextLine()){
-                String line=sc.nextLine();
-                System.out.println(line);
-            }
-            
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found "+e);
+        
+        List<Integer> list=new ArrayList<Integer>();
+
+        list.add(5);
+        list.add(10);
+        list.add(25);
+        list.add(14);
+        list.add(3);
+
+        for(int i:list){
+            System.out.println(i);
         }
 
-        // taking input from a string
-        String str="123 78 5896 147";
-        try {
-            Scanner sc=new Scanner(str);
-            while(sc.hasNextLine()){
-                int num=sc.nextInt();
-                System.out.println(num);
-            }
-            
-        } catch (Exception e) {
-            System.out.println("A weird exception "+e);
-        }
+        System.out.println("\n"+list.get(2)); // use List to get methods like get() to fetch ny index value
+
+        System.out.println("\nIndex of "+list.get(3)+" is "+list.indexOf(14));
+
+        /*System.out.println("\n");
+
+        Collection list1=new ArrayList();
+
+        list1.add(5);
+        list1.add(10);
+        list1.add(25);
+        list1.add("bhaawan");   // will not give compile time error but will give exception at run-time
+
+        for(Object i:list1){
+            int num=(Integer)i;
+            System.out.println(num*2);
+        }*/
     }
 }

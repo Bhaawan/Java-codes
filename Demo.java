864ea509e14e7f3a141cb8085421004185daf799
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-09-13 23:23:10 +0530

    EP-97

--- Demo.java
+++ Demo.java
@@ -1,21 +1,12 @@
 import java.util.*;
+import java.util.function.Consumer;
 
 public class Demo {
     public static void main(String args[]) {
 
         List<Integer> nums=Arrays.asList(4,7,5,8,9,6,2);
 
-        // 1st way of doing this
-
-        int sum=0;
-        for(int i:nums){
-            if(i%2==0){
-                i*=2;
-                sum+=i;
-            }
-        }
-
-        System.out.println("value of sum = "+sum);
+        nums.forEach(a->System.out.print(a+" "));
 
     }
 

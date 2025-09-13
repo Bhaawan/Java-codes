2a27ca2e991d5918dded53c16c4934702fd21e7f
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-09-13 22:45:38 +0530

    EP-96

--- Demo.java
+++ Demo.java
@@ -1,66 +1,22 @@
-
 import java.util.*;
-
-class Students implements Comparable<Students>{
-    String name;
-    int age;
-
-    Students(String name, int age){
-        this.name=name;
-        this.age=age;
-    }
-
-    @Override
-    public String toString(){
-        return "Student [age = "+age+", name = "+name+"]";
-    }
-
-    public int compareTo(Students that){
-        if(this.age>that.age){
-            return 1;
-        }
-        
-        return -1;
-    }
-}
 
 public class Demo {
     public static void main(String args[]) {
 
-        Comparator<String> comp=(String s1, String s2)->{
-                return (s1.length() - s2.length())*-1;
-            };
+        List<Integer> nums=Arrays.asList(4,7,5,8,9,6,2);
 
-        List<String> list = new ArrayList<>();
-        list.add("bhaawan");
-        list.add("kumar");
-        list.add("sin");
-        list.add("gautam");
-        
-        System.out.println(list);
+        // 1st way of doing this
 
-        Collections.sort(list, comp);
-
-        System.out.println(list);
-    
-        List<Students> students = new ArrayList<>();
-        students.add(new Students("bhaawan", 20));
-        students.add(new Students("kumar", 22));
-        students.add(new Students("sin", 21));
-        students.add(new Students("gautam", 19));
-        
-        for(Students s:students){
-            System.out.println(s);
+        int sum=0;
+        for(int i:nums){
+            if(i%2==0){
+                i*=2;
+                sum+=i;
+            }
         }
-        System.out.println();
-        
-        Collections.sort(students);
-        
-        for(Students s:students){
-            System.out.println(s);
-        }
-    
-    
+
+        System.out.println("value of sum = "+sum);
+
     }
 
 }
